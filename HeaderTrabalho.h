e52020aa5f5698e020a8aad5bf031b0de175f36d
ogeidexe <ogeidexe@gmail.com>
2018-06-13 10:26:55 -0300

    Add files via upload

---
+++ HeaderTrabalho.h
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#include <time.h>
+
+int geraValores(int vet[],int x){
+    int i;
+    printf("gerando valores aleatórios na faixa de 0 a %d \n", x);
+    //Reseta o tempo no programa para que os valores sejão aleatorios para cada execução 
+    srand(time(NULL));
+    for (i=0; i < x; i++)
+     {  
+           vet[i]=rand() % (x);
+     }
+}
+int menu()
+{
+    int x;
+    puts("Selecione o caso para teste");
+    puts("1.Caso 1 com 5000 elementos");
+    puts("2.Caso 2 com 50000 elementos");
+    puts("3.Caso 3 com 500000 elementos");
+    puts("4.ver tabela");
+    puts("0.Sair");
+    scanf("%d",&x);
+    if(x==0)exit(0);
+    return x;
+}
+
+void imprimVetor(int vet[], int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d. %d\n",i,vet[i]);
+    }
+
+}
+
+//Método da Bolha---------------------------------------------------------------------
+double bubleSort(int vet[], int x)
+{
+    puts("ordenado metodo bolha");
+    int i,j,aux;
+    double resultado=0;
+    j=x;
+    #pragma omp parallel for 
+    for(i=0;i<x-1;i++)
+    {
+        #pragma omp parallel for //13034.666000
+        for(j=1;j<x-i;j++)
+        {
+            if(vet[j]>vet[j+1])
+            {
+                aux = vet[j];
+                vet[j]=vet[j+1];
+                vet[j+1]=aux;
+                resultado++;
+            }
+        }
+
+    }
+    return resultado;
+}
+//Método da Bolha melhorado-------------------------------------------------------
+double bubleSortM(int vet[], int x)
+{
+    puts("ordenado metodo bolha melhorado");
+    int i,j,aux,troucou;
+    double resultado=0;
+    j=x;
+    for(i=0;i<x-1;i++)
+    {
+        troucou=0;
+        for(j=1;j<x-i;j++)
+        {
+            if(vet[j]>vet[j+1])
+            {
+                aux = vet[j];
+                vet[j]=vet[j+1];
+                vet[j+1]=aux;
+                troucou=1;
+                resultado++;
+            }
+        }
+        if(troucou==0)break;
+
+    }
+    return resultado;
+}
+//Método de Ordenação Troca e Partição-------------------------------------------------
+quickSort(int vet[], int x)
+{
+    float resultado;
+    return resultado;
+}
+//Método de Ordenação Inserção Direta-----------------------------------------------------
+inserctSort(int vet[], int x)
+{
+    float resultado;
+    return resultado;
+}
+//Método de Ordenação por Incrementos decrecentes---------------------------------------
+shellSort(int vet[], int x)
+{
+    float resultado;
+    return resultado;
+}
+//Método de Ordenação por inserção direta------------------------------------------------------
+selectSort(int vet[], int x)
+{
+    float resultado;
+    return resultado;
+}
+//Método de Ordenação tipo arvore----------------------------------------------------------------
+heapSort(int vet[], int x)
+{
+    float resultado;
+    return resultado;
+}
+//função para apresentar os resultados de cada interação-------------------------------
+void resultado(double vet[],int tamResultado)
+{
+    
+    for(int i = 0; i < tamResultado; i++)
+    {
+        printf("|Tempo|%lf ||  \n");
+    }
+    
+}
---
+++ Trabalho.c
@@ -0,0 +1,81 @@
+#include "HeaderTrabalho.h"
+#define x3 5000
+#define x4 50000
+#define x5 500000
+
+int main()
+{
+    int x,v1[x3],v2[x4],v3[x5];
+    float processador[7];
+    puts("metodos de Ordenação");
+    geraValores(v1,x3);
+    geraValores(v2,x4);
+    geraValores(v3,x5);
+    //recebe o apção da função menu
+    x=menu();
+     switch(x)
+     {
+         case 1:
+             //Método da Bolha 
+            processador[0] = bubleSort(v1,x3);
+             //  Método da Bolha melhorado 
+            processador[1] = bubleSortM(v1,x3);
+             //Método de Ordenação Troca e Partição 
+            // processador[2] = quickSort(v1);
+            //Método de Ordenação Inserção Direta 
+    //         processador[3] = inserctSort(v1);
+    //         //Método de Ordenação por Incrementos decrecentes 
+    //         processador[4] = shellSort(v1);
+    //         //Método de Ordenação por inserção direta
+    //         processador[5] = selectSort(v1);
+    //         //Método de Ordenação tipo arvore
+    //         processador[6] = heapSort(v1);
+    //         break;
+    //     case 2:
+    //         //Método da Bolha 
+    //         processador[0] = bubleSort(v2);
+    //          //Método da Bolha melhorado 
+    //         processador[1] = bubleSortM(v2);
+    //         //Método de Ordenação Troca e Partição 
+    //         processador[2] = quickSort(v2);
+    //         //Método de Ordenação Inserção Direta 
+    //         processador[3] = inserctSort(v2);
+    //         //Método de Ordenação por Incrementos decrecentes 
+    //         processador[4] = shellSort(v2);
+    //         //Método de Ordenação por inserção direta
+    //         processador[5] = selectSort(v2);
+    //         //Método de Ordenação tipo arvore
+    //         processador[6] = heapSort(v2);
+    //         break;
+    //     case 3:
+    //         //Método da Bolha 
+    //         processador[0] = bubleSort(v3);
+    //          //Método da Bolha melhorado 
+    //         processador[1] = bubleSortM(v3);
+    //         //Método de Ordenação Troca e Partição 
+    //         processador[2] = quickSort(v3);
+    //         //Método de Ordenação Inserção Direta 
+    //         processador[3] = inserctSort(v3);
+    //         //Método de Ordenação por Incrementos decrecentes 
+    //         processador[4] = shellSort(v3);
+    //         //Método de Ordenação por inserção direta
+    //         processador[5] = selectSort(v3);
+    //         //Método de Ordenação tipo arvore
+    //         processador[6] = heapSort(v3);
+    //         break;
+    //     case 4:
+            
+    //         break;
+    //     case 5:
+            
+    //         break;
+    //     case 6:
+            
+    //         break; 
+    //     case 7:
+            
+    //         break;
+    // }
+    return 0;
+}
+ 
